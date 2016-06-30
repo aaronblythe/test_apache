87e048dd30f2e5bf20e4311f653532c0ad6f01df
Aaron Blythe <ablythe@hearst.com>
2016-06-30 16:15:16 -0500

    networking two machines using test-kitchen

--- attributes/default.rb
+++ attributes/default.rb
@@ -4,4 +4,5 @@
 #
 # Copyright (c) 2016 The Authors, All Rights Reserved.
 
-default['my_app']['name'] = 'test_application'
+default['my_app']['name'] = 'hello_app'
+default['apache']['docroot_dir'] = '/var/www/html'
--- recipes/default.rb
+++ recipes/default.rb
@@ -8,3 +8,10 @@ web_app "my_app" do
   template 'web_app.conf.erb'
   server_name node['my_app']['name']
 end
+
+template '/var/www/html/index.html' do
+  source 'index.html.erb'
+  owner 'root'
+  group 'root'
+  mode '0755'
+end
